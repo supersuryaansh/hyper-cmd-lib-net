dd5f3adc2098579c54cee14f4fb15b4ef33a55c1
prdn <paolo.ardoino@gmail.com>
2022-11-19 13:43:40 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+function connHandler(connection, _dst, opts = {}, stats = {}) {
+  const loc = _dst()
+  if (loc === null) {
+    connection.destroy() // don't return rejection error
+
+    if (!stats.rejectCnt) {
+      stats.rejectCnt = 0
+    }
+
+    stats.rejectCnt++
+
+    return
+  }
+
+  if (!stats.locCnt) {
+    stats.locCnt = 0
+  }
+
+  if (!stats.remCnt) {
+    stats.remCnt = 0
+  }
+
+  stats.locCnt++
+  stats.remCnt++
+
+  let destroyed = false
+
+  loc.pipe(connection).pipe(loc)
+
+  loc.on('error', destroy)
+  loc.on('close', destroy)
+
+  connection.on('error', destroy)
+  connection.on('close', destroy)
+
+  loc.on('connect', err => {
+    if (opts.debug) {
+      console.log('connected')
+    }
+  }).on('error', err => {
+    if (opts.debug) {
+      console.error(err)
+    }
+  }).on('close', () => {
+    stats.locCnt--
+  })
+
+  connection.on('error', err => {
+    if (opts.debug) {
+      console.error(err)
+    }
+  }).on('close', () => {
+    stats.remCnt--
+  })
+
+  function destroy (err) {
+    if (destroyed) return
+    destroyed = true
+    loc.destroy(err)
+    connection.destroy(err)
+  }
+}
+
+module.exports = {
+  connHandler: connHandler
+}
